196850b47c06543ba86f248cefbeb82fc918afda
sryngtys22 <126160963+sryngtys22@users.noreply.github.com>
2025-03-22 04:23:25 +0700

    Update superior_whyred.mk

--- superior_whyred.mk
+++ superior_whyred.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := superior_whyred
+PRODUCT_NAME := aosp_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
